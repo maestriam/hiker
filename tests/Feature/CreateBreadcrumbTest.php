7319fa83e2cca71f2a45aff1da1701eef26f0f5f
Giuliano Sampaio <giuguitar@gmail.com>
2020-07-23 15:46:55 -0300

    teste simples de breadcrumb

---
+++ tests/Feature/CreateBreadcrumbTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Maestriam\Hiker\Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Route;
+use Maestriam\Hiker\Traits\Support\Hikeable;
+
+class CreateBreadcumbTest extends TestCase
+{
+    use Hikeable;
+
+    public function testCreateBreadcrumb()
+    {
+        Route::get('/', [
+            'as'  => 'test.index',
+        ]);
+
+        Route::get('/show', [
+            'as'  => 'test.show',
+        ]);
+
+        $breadcrumb = $this->hiker()
+                           ->breadcrumb('test.show');            
+    
+        $breadcrumb->push('test.index')
+                   ->push('test.show');
+
+        foreach ($breadcrumb->collection as $route) {
+            $this->assertIsString($route->url);            
+        }
+    }
+}
